22891633e4971bee191ca72844f66cc48638b24a
David Malmström <syntex@outlook.com>
2016-06-14 10:57:53 +0200

    Add project files.

---
+++ MVCIntro2-20160614/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCIntro2_20160614.MvcApplication" Language="C#" %>
---
+++ MVCIntro2-20160614/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCIntro2_20160614.Startup))]
+namespace MVCIntro2_20160614
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
